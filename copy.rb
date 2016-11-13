7db585d8dce9866ce91ecfce6444ce7243ea371e
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-13 13:31:11 +0200

    Fix file name

--- copy.rb
+++ copy.rb
@@ -6,5 +6,8 @@ platforms = ["ios", "macos", "tvos", "watchos"]
 puts "Copying to SwiftyZeroMQ..."
 
 for platform in platforms
-  FileUtils.cp "dist/#{platform}/lib/libzmq.a", "../SwiftyZeroMQ/Sources/"
+  FileUtils.cp(
+    "dist/#{platform}/lib/libzmq.a",
+    "../SwiftyZeroMQ/Sources/libzmq-#{platform}.a"
+  )
 end
