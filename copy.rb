edd2769e97828d73cbf5afa111a835fed67e2665
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-13 13:29:45 +0200

    Add copy script

---
+++ copy.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+platforms = ["ios", "macos", "tvos", "watchos"]
+puts "Copying to SwiftyZeroMQ..."
+
+for platform in platforms
+  FileUtils.cp "dist/#{platform}/lib/libzmq.a", "../SwiftyZeroMQ/Sources/"
+end
